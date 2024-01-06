e525cbd38d36d62c34f375c9458dd1d6b99b5c29
Emmanuel <empiredalex@gmail.com>
2024-01-07 00:18:29 +0100

    initial commit

--- app/(root)/portfy/page.tsx
+++ app/(root)/portfy/page.tsx
@@ -22,9 +22,9 @@ const Portfy = () => {
         <section className='w-[100%]'>
           <div className='w-[100%] flex flex-col gap-5'>
             <div className='bg-white flex items-center text-black p-5'>
-              <div className='w-[50%] flex flex-col gap-4'>
+              <div className='w-[50%] flex flex-col gap-5'>
                 <h1 className='text-4xl font-bold'>Reliable</h1>
-                <p className='text-xl font-medium'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta excepturi placeat distinctio eligendi alias.</p>
+                <p className='text-xl font-normal'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta excepturi placeat distinctio eligendi alias.</p>
                 <div className=' bg-slate-400 w-[30%] flex justify-center p-3 rounded-md cursor-pointer'>
                   <button>
                     Contact
@@ -33,28 +33,29 @@ const Portfy = () => {
               </div>
               <Image src="/eman.jpeg" alt='header' width={100} height={100} className='w-[50%] h-[20vh]'/>
             </div>
-            <div className='bg-white text-black p-5 flex-col gap-3 flex'>
+            <div className='bg-white text-black p-5 flex-col gap-5 flex'>
               <h1 className='text-4xl font-bold'>Front-end Skills</h1>
-              <div className='flex flex-col gap-5'>
+              <div className='flex flex-col gap-7'>
                 <div>
                   <h1 className='text-2xl font-semibold'>UI/UX Design</h1>
-                  <p className='text-xl font-medium'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, iure nesciunt. Sit quisquam dolorum voluptatibus perspiciatis iure recusandae quam qui! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque, doloremque.</p>
+                  <p className='text-xl font-normal'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, iure nesciunt. Sit quisquam dolorum voluptatibus perspiciatis iure recusandae quam qui! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque, doloremque.</p>
                 </div>
                 <div>
                   <h1 className='text-2xl font-semibold'>HTML, CSS, JAVASCRIPT</h1>
-                  <p className='text-xl font-medium'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, iure nesciunt. Sit quisquam dolorum voluptatibus perspiciatis iure recusandae quam qui! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque, doloremque.</p>
+                  <p className='text-xl font-normal'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, iure nesciunt. Sit quisquam dolorum voluptatibus perspiciatis iure recusandae quam qui! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque, doloremque.</p>
                 </div>
                 <div>
                   <h1 className='text-2xl font-semibold'>REACT FRAME-WORKS</h1>
-                  <p className='text-xl font-medium'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, iure nesciunt. Sit quisquam dolorum voluptatibus perspiciatis iure recusandae quam qui! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque, doloremque.</p>
+                  <p className='text-xl font-normal'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, iure nesciunt. Sit quisquam dolorum voluptatibus perspiciatis iure recusandae quam qui! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque, doloremque.</p>
                 </div>
               </div>
             </div>
-            <div className='bg-white text-black flex flex-col items-center p-4 h-[30vh]'>
+            <div className='bg-white text-black flex flex-col items-center p-4 h-[30vh] w-[100%]'>
               <h1 className='text-4xl font-bold'>Certificate</h1>
               <div className='w-[100%] h-[40vh] flex items-center gap-2'>
-                <Image src="/certi.png" alt='certi' width={100} height={100} className='w-[50%] h-[20vh]'/>
-                <Image src="/certi.png" alt='certi' width={100} height={100} className='w-[50%] h-[20vh]'/>
+                <Image src="/certi.png" alt='certi' width={100} height={100} className='w-[30%] h-[20vh]'/>
+                <Image src="/funds.png" alt='certi' width={100} height={100} className='w-[30%] h-[20vh]'/>
+                <Image src="/advance.png" alt='certi' width={100} height={100} className='w-[30%] h-[20vh]'/>
               </div>
             </div>
           </div>
