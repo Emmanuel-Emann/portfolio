46ffbe0273c756ef592d60905257fed1e3672b24
Emmanuel <empiredalex@gmail.com>
2024-02-13 04:52:57 +0100

    initial commit

--- components/about/Abouttext.tsx
+++ components/about/Abouttext.tsx
@@ -9,8 +9,8 @@ const Abouttext = () => {
   }
   return (
   <div className='flex items-center gap-5 flex-col w-[100%] transparentglass justify-center'>
-    <h1>
-      About Me
+    <h1 className='text-4xl font-bold font-mono'>
+      About
     </h1>
     <div>
       {isReadMore ? <p className='font-mono text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam repudiandae atque vel libero odio ab maxime eum ut, quasi quo totam doloremque? Nam, corrupti quisquam tempore exercitationem voluptate atque nisi facere enim excepturi culpa nobis. Quaerat illo debitis expedita cum porro a. Sapiente nesciunt quis qui ab eius, dolor dolore. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero rem sapiente maiores natus aliquam veniam ipsum suscipit delectus! Nesciunt labore, nisi esse qui, possimus eos distinctio debitis ad, iste error voluptates neque laudantium expedita tempore impedit sed velit provident dignissimos placeat culpa repellat beatae. Voluptatum voluptatem quas inventore accusamus molestias!</p> : <p className='font-mono text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam repudiandae atque vel libero odio ab maxime eum ut, quasi quo totam doloremque? Nam, corrupti quisquam tempore exercitationem voluptate atque nisi facere enim excepturi culpa nobis. Quaerat illo debitis expedita cum porro a. Sapiente nesciunt quis qui ab eius, dolor dolore.</p>}
